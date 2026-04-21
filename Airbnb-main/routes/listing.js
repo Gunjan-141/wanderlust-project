6efb0d63ba783695fa556ec3ea8cccf6368a757d
Gunjan Modi <mgunjan041@gmail.com>
2026-04-21 22:12:22 +0530

    Initial commit

---
+++ Airbnb-main/routes/listing.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const Listing = require("../models/listing.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const { isLoggedIn, isOwner, validateListing} = require("../middleware.js");
+const listingController = require("../controllers/listing.js");
+const multer  = require('multer');
+const { storage } = require('../cloudConfig.js');
+const upload = multer({ storage });
+
+router
+   .route("/")
+   .get(wrapAsync(listingController.index)) //index route
+   .post(isLoggedIn, upload.single("listing[image]"), validateListing, wrapAsync(listingController.createNewListing)); //create route
+   
+
+   
+//New Route ==> server get confused so that it is before show route
+router.get("/new",isLoggedIn, listingController.renderNewForm);
+
+
+router
+   .route("/:id")
+   .get(wrapAsync(listingController.showListing)) // show route
+   .put(isLoggedIn, isOwner,upload.single("listing[image]"), validateListing, wrapAsync(listingController.updateListing)) //update route
+   .delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListing)); //delete route
+
+
+//Edit Route
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
+
+
+module.exports = router;
